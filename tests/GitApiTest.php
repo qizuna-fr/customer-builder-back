9905f3584481fade11cebdc26ab2d8fdcc06133d
david brylka <gt-turbo@jagware.org>
2022-05-10 09:24:11 +0200

    GitApiTest creation et phpunit installation

---
+++ tests/GitApiTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class GitApiTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
