0f321c1013d633a39ef993d01c9554fb677cee1f
david brylka <gt-turbo@jagware.org>
2022-05-24 11:35:42 +0200

    Mise en place du dispatcher dans le controlleur, avec recuperation des infos clients

--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -9,7 +9,10 @@ use App\Interfaces\GitHubServiceInterface;
 use App\Interfaces\HubspotServiceInterface;
 use App\Interfaces\ImaginaryServiceInterface;
 use App\Interfaces\TextCSSManagementInterface;
+use App\Services\DataBaseManagement;
 use App\Services\Dummy\DummyHubspotService;
+use App\Services\Dummy\DummyDataBaseManagement;
+use App\Services\PennyLaneService;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -20,98 +23,168 @@ class ServicesController extends AbstractController
 {
 
     private $data = [];
-    public function __construct(private GitHubServiceInterface $github, private FormattingTextInterface $formattingText, 
-    private TextCSSManagementInterface $textCSSManagement, private ImaginaryServiceInterface $imaginary, 
-    private DataBaseManagementInterface $dataBase, private ClientInterface $client, private HubspotServiceInterface $hubspot)
+    private $clientData = [];
+    private string $cityName;
+
+    public function __construct(
+        private GitHubServiceInterface $github,
+        private FormattingTextInterface $formattingText,
+        private TextCSSManagementInterface $textCSSManagement,
+        private ImaginaryServiceInterface $imaginary,
+        private DataBaseManagementInterface $clientDataBase,
+        private ClientInterface $client,
+        private HubspotServiceInterface $hubspot,
+        private PennyLaneService $pennyLane
+    ) {
+        // $this->data = $this->dataBase->fetchDataFromDataBase($this->client->getClientName());
+        $dataBase = new DummyDataBaseManagement();
+        $this->clientData = $dataBase->fetchDataFromDataBase($this->client->getClientName());
+    }
+
+    private function createFile($clientData)
     {
-        $this->data = $this->dataBase->fetchDataFromDataBase($this->client->getClientName());
-
+        return new JsonResponse($clientData);
     }
 
-    private function createFile($data){
-        return new JsonResponse($data);
+    #[Route('/qizuna-dispatcher')]
+    public function qizuna_dispatcher()
+    {
+        echo ("Données du client : ");
+        var_dump($this->clientData);
+        echo ("<br>");
+
+        $this->cityName = $this->clientData["cityName"];
+        $this->titleFont = $this->clientData["titleFont"];
+        $this->titleColor = $this->clientData["titleColor"];
+        $this->titleStyle = $this->clientData["titleStyle"];
+        $this->paragraphFont = $this->clientData["paragraphFont"];
+        $this->paragraphColor = $this->clientData["paragraphColor"];
+        $this->paragraphStyle = $this->clientData["paragraphStyle"];
+        $this->clientFileOne = $this->clientData["clientFiles"]["clientLogo"];
+        $this->clientFileOne = $this->clientData["clientFiles"]["clientBackground"];
+
+        echo ("<br> <br>");
+
+        echo ("Appel de formatCityName :");
+        $this->formatCityName();
+
+        echo ("Appel de defineFont :");
+        $this->defineFont(0, 0);
+
+        echo ("Appel de defineStyle :");
+        $this->defineStyle(0, 0);
+
+        echo ("Appel de defineColor :");
+        $this->defineColor(0, 0);
+
+        echo ("Appel de creatPennyLaneClient :");
+        // $this->createPennyLaneClient($clientName);
+
+
+        return new Response("Qizuuunnnnaaaa");
     }
-    
+
     #[Route('/format-city-name')]
     public function formatCityName(): Response
     {
-        $formattedCityName = $this->formattingText->deleteSpace($this->data["cityName"]);
+        echo ("formatCityName appellé <br>");
 
-        $branchName = "formatted-cityName";
-        $message = "formatting city name for ".$this->data["cityName"];
-        $file = $this->createFile(array('cityName' => $formattedCityName));
-        $this->github->pushFileToGithub($file, $branchName, $message);
-        
+        // $formattedCityName = $this->formattingText->deleteSpace($this->data["cityName"]);
+
+        // $branchName = "formatted-cityName";
+        // $message = "formatting city name for " . $this->data["cityName"];
+        // $file = $this->createFile(array('cityName' => $formattedCityName));
+        // $this->github->pushFileToGithub($file, $branchName, $message);
+
         return new Response("Formatting city name", 200);
     }
 
     #[Route('/edit-font/{text}/{font}')]
     public function defineFont($text, $font): Response
     {
-        if (! in_array($text.'Font', $this->data)) throw new Exception($text."Font n'existe pas !");
-        $textName = $this->data[$text.'Font'];
-        $this->textCSSManagement->editTextFont($textName, $font);
-        $file = $this->createFile(array($textName => $font));
+        echo ("defineFont appellé <br>");
 
-        $branchName = "edit-font-".$textName;
-        $message = "editing ".$textName." font for ".$this->data["cityName"];
-        $this->github->pushFileToGithub($file, $branchName, $message);
+        // if (!in_array($text . 'Font', $this->data)) throw new Exception($text . "Font n'existe pas !");
+        // $textName = $this->data[$text . 'Font'];
+        // $this->textCSSManagement->editTextFont($textName, $font);
+        // $file = $this->createFile(array($textName => $font));
 
-        return new Response('Editing '.$text.' font to '.$font, 200);
+        // $branchName = "edit-font-" . $textName;
+        // $message = "editing " . $textName . " font for " . $this->data["cityName"];
+        // $this->github->pushFileToGithub($file, $branchName, $message);
+
+        return new Response('Editing ' . $text . ' font to ' . $font, 200);
     }
 
     #[Route('/edit-style/{text}/{style}')]
     public function defineStyle($text, $style): Response
-    { 
-        if (! in_array($text.'Style', $this->data)) throw new Exception($text."Style n'existe pas !");
-        $textName = $this->data[$text.'Style'];
-        $this->textCSSManagement->editTextStyle($textName, $style);
-        $file = $this->createFile(array($textName => $style));
+    {
+        echo ("defineStyle appellé <br>");
 
-        $branchName = "edit-style-".$textName;
-        $message = "editing ".$textName." style for ".$this->data["cityName"];
-        $this->github->pushFileToGithub($file, $branchName, $message);
+        // if (!in_array($text . 'Style', $this->data)) throw new Exception($text . "Style n'existe pas !");
+        // $textName = $this->data[$text . 'Style'];
+        // $this->textCSSManagement->editTextStyle($textName, $style);
+        // $file = $this->createFile(array($textName => $style));
 
-        return new Response('Editing '.$text.' style controller', 200);
+        // $branchName = "edit-style-" . $textName;
+        // $message = "editing " . $textName . " style for " . $this->data["cityName"];
+        // $this->github->pushFileToGithub($file, $branchName, $message);
+
+        return new Response('Editing ' . $text . ' style controller', 200);
     }
-    
+
     #[Route('/edit-color/{text}/{style}')]
     public function defineColor($text, $color): Response
     {
+        echo ("defineColor appellé <br>");
 
-        if (! in_array($text.'Color', $this->data)) throw new Exception($text."Color n'existe pas !");
-        $textName = $this->data[$text.'Color'];
-        $this->textCSSManagement->editTextColor($textName, $color);
-        $file = $this->createFile(array($textName => $color));
+        // if (!in_array($text . 'Color', $this->data)) throw new Exception($text . "Color n'existe pas !");
+        // $textName = $this->data[$text . 'Color'];
+        // $this->textCSSManagement->editTextColor($textName, $color);
+        // $file = $this->createFile(array($textName => $color));
 
-        $branchName = "edit-color-".$textName;
-        $message = "editing ".$textName." color for ".$this->data["cityName"];
-        $this->github->pushFileToGithub($file, $branchName, $message);
+        // $branchName = "edit-color-" . $textName;
+        // $message = "editing " . $textName . " color for " . $this->data["cityName"];
+        // $this->github->pushFileToGithub($file, $branchName, $message);
 
-        return new Response('Editing '.$text.' color controller', 200);
+        return new Response('Editing ' . $text . ' color controller', 200);
     }
 
     #[Route('/resize/{clientFile}/{width}/{hight}')]
     public function resizeImage($width, $hight, $clientFile): Response
     {
-        $file = $this->data['clientFiles'][$clientFile];
-        $this->imaginary->resizeImage($file, $width, $hight);
+        echo ("resizeImage appellé <br>");
 
-        $branchName = "resize-image";
-        $message = "resizing image for ".$this->data["cityName"];
-        $fileToPush = $this->createFile(array($clientFile => array('width' => $width, 'hight' => $hight)));
+        // $file = $this->data['clientFiles'][$clientFile];
+        // $this->imaginary->resizeImage($file, $width, $hight);
 
-        $this->github->pushFileToGithub($fileToPush, $branchName, $message);
-        
-        return new Response('resizing image '.$clientFile, 200);
+        // $branchName = "resize-image";
+        // $message = "resizing image for " . $this->data["cityName"];
+        // $fileToPush = $this->createFile(array($clientFile => array('width' => $width, 'hight' => $hight)));
+
+        // $this->github->pushFileToGithub($fileToPush, $branchName, $message);
+
+        return new Response('resizing image ' . $clientFile, 200);
     }
 
     #[Route('/create-client-hubspot/{clientName}/{clientMail}')]
     public function createHubspotClient($clientName, $clientMail): Response
     {
-        $client = $this->dataBase->clientToAdd($clientName, $clientMail);
-        $this->hubspot->createClient($client);
+        echo ("createHubSpotClient appellé");
 
-        return new Response('Creating hubspot client for '.$clientName.' '.$clientMail, 200);
+        // $client = $this->dataBase->clientToAdd($clientName, $clientMail);
+        // $this->hubspot->createClient($client);
+
+        return new Response('Creating hubspot client for ' . $clientName . ' ' . $clientMail, 200);
+    }
+
+    public function createPennyLaneClient($clientName): Response
+    {
+        echo ("createPennyLaneClient appellé <br>");
+
+        // $client = $this->dataBase->clientToAdd($clientName, $clientMail);
+        // $this->hubspot->createClient($client);
+
+        return new Response('Creating PennyLane client ');
     }
 }
--- tests/ServicesTest.php
+++ tests/ServicesTest.php
@@ -9,6 +9,8 @@ use App\Services\GitHubService;
 use App\Services\ImaginaryService;
 use App\Services\TextCSSManagement;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Services\Dummy\DummyClient;
+use App\Services\PennyLaneService;
 
 class ServicesTest extends WebTestCase
 {
@@ -115,6 +117,29 @@ class ServicesTest extends WebTestCase
             $service->createClient($dummyDataBase->clientToAdd($clientName, $clientMail));
         }
         $this->assertSame($service->spycreate, true);
+    }
+
+    public int $client_id=0;
+
+    public function testPennyLaneServiceCreate(): void
+    {
+        $client = new DummyClient();
+        $clientAirtable = $client->getAirTableClient();
+
+        $pennylane = new PennyLaneService();
+        $this->client_id = $pennylane->create($clientAirtable);
+        $this->assertNotNull($this->client_id, "Client_id error");
+        $this->getExpectedExceptionMessage("Client yet created");
+        $this->getExpectedExceptionMessage("Authentification error");
+    }
+
+    public function testPennyLaneServiceSubscribe(): void
+    {
+        $pennylane = new PennyLaneService();
+        $subscribe = $pennylane->subscribe($this->client_id);
+        $this->assertTrue($subscribe, "Subscription error");
+        $this->getExpectedExceptionMessage("Authentification error");
+
     }
 
 }
