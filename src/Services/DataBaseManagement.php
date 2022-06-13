6512bc943323ef2032784e0f1478f95364b99c77
meriam <meriam.mahjoub@gmail.com>
2022-06-13 10:57:34 +0200

    nettoyage

--- src/Services/DataBaseManagement.php
+++ src/Services/DataBaseManagement.php
@@ -2,7 +2,6 @@
 namespace App\Services;
 
 use App\Interfaces\DataBaseManagementInterface;
-use Yoanbernabeu\AirtableClientBundle\AirtableClientInterface;
 
 class DataBaseManagement implements DataBaseManagementInterface {
     
@@ -11,37 +10,15 @@ class DataBaseManagement implements DataBaseManagementInterface {
     }
 
     public function fetchCustomerByCityName(string $cityName) : Customer{
-        // $clientRecords = $this->airtable->findBy('Client', 'CityName', $cityName);
-        // foreach($clientRecords as $record) {
-        //     $client = $record->getFields();
-        // }
 
         $url = "https://api.airtable.com/v0/app9QhNsv5170O8Iw/Client?filterByFormula=SEARCH('".$cityName."'%2C+%7BcityName%7D)";
 
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $resp = $this->getCurlResponse($url);
         
-        $headers = array(
-           "Authorization: Bearer keyWdc5YHi3Jwi34f",
-        );
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-        //for debug only!
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        
-        $resp = curl_exec($curl);
-        curl_close($curl);
         $jsonObject = json_decode($resp);
         $clientRecords = $jsonObject->records[0]->fields;
         // dd($jsonObject->records[0]->fields);
         $clientDataId = $clientRecords->Data;
-        $clientDatas = [];
-        // dd($clientDataId);
-        // foreach ($clientDataId as $id){
-            // $data = $this->getClientData($id);
-            // array_push($clientDatas, $data);
-        // }
         $client = new Customer($clientRecords->Id, $clientRecords->CityName, $clientRecords->Email, $clientDataId);
         
         return $client;
@@ -51,20 +28,7 @@ class DataBaseManagement implements DataBaseManagementInterface {
 
         $url = "https://api.airtable.com/v0/app9QhNsv5170O8Iw/Client";
 
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-        $headers = array(
-        "Authorization: Bearer keyWdc5YHi3Jwi34f",
-        );
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-        //for debug only!
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-
-        $resp = curl_exec($curl);
-        curl_close($curl);
+        $resp = $this->getCurlResponse($url);
 
         $jsonObject = json_decode($resp);
         $clientRecords = $jsonObject->records;
@@ -75,18 +39,7 @@ class DataBaseManagement implements DataBaseManagementInterface {
         // dd($id);
         $url = "https://api.airtable.com/v0/app9QhNsv5170O8Iw/Data/".$id; 
         // $url = "https://api.airtable.com/v0/app9QhNsv5170O8Iw/Data?filterByFormula=SEARCH('".$id."'%2C+%7BClient%7D)";
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $headers = array(
-        "Authorization: Bearer keyWdc5YHi3Jwi34f",
-        );
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-        //for debug only!
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        $resp = curl_exec($curl);
-        curl_close($curl);
+        $resp = $this->getCurlResponse($url);
         $jsonObject = json_decode($resp);
         $data = $jsonObject->fields;
 
@@ -106,6 +59,7 @@ class DataBaseManagement implements DataBaseManagementInterface {
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         $resp = curl_exec($curl);
         curl_close($curl);
+        return $resp;
     }
 
     public function persist(Customer $customer){
