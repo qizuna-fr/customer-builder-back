5fe8e2edc27d743d54b3a9b336277c096892aa06
meriam <meriam.mahjoub@gmail.com>
2022-06-13 09:21:39 +0200

    git pour merge

--- src/Services/DataBaseManagement.php
+++ src/Services/DataBaseManagement.php
@@ -93,6 +93,21 @@ class DataBaseManagement implements DataBaseManagementInterface {
         return $data;
     }
 
+    private function getCurlResponse(string $url) {
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $headers = array(
+        "Authorization: Bearer keyWdc5YHi3Jwi34f",
+        );
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        //for debug only!
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        $resp = curl_exec($curl);
+        curl_close($curl);
+    }
+
     public function persist(Customer $customer){
         
     }
